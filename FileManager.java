1539c7b730093139e6d9f0800573db96efb68f11
Shamal47770 <daurenbekenov237@gmail.com>
2026-03-31 18:59:33 +0600

    Create FileManager.java

---
+++ FileManager.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class FileManager {
+
+    public static void saveStudent(Student s) {
+        try {
+            FileWriter writer = new FileWriter("students.txt", true);
+            writer.write(s.getName() + "," + s.getId() + "\n");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Error saving file");
+        }
+    }
+
+    public static ArrayList<Student> loadStudents() {
+        ArrayList<Student> list = new ArrayList<>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("students.txt"));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                String name = parts[0];
+                String id = parts[1];
+
+                list.add(new Student(name, id));
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("No file yet");
+        }
+
+        return list;
+    }
+}
