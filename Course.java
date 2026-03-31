b66de3d6d505eed62b5f8544129ce89064b9cad9
Shamal47770 <daurenbekenov237@gmail.com>
2026-03-31 19:56:05 +0600

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,8 +1,10 @@
 public class Course {
     private String courseName;
-  public Course(String courseName) {
+
+    public Course(String courseName) {
         this.courseName = courseName;
     }
+
     public String getCourseName() {
         return courseName;
     }
