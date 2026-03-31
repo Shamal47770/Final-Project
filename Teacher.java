99f1a5d0c4a34d2ab33f4f154f5a571ce2c647b9
Shamal47770 <daurenbekenov237@gmail.com>
2026-03-31 19:00:26 +0600

    Create Teacher.java

---
+++ Teacher.java
@@ -0,0 +1,9 @@
+public class Teacher extends User {
+public Teacher(String name, String id) {
+    super(name, id);
+}
+ @Override
+    public void displayInfo() {
+        System.out.println("Teacher: " + name + " ID: " + id);
+    }
+}
